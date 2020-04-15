928902304836a30bbf82b2b3ea4cc0c8101cd325
c14180100 <c14180100@john.petra.ac.id>
2020-04-15 10:06:11 +0700

    2 files modified - unfinished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,29 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   name = 'Angular';
 
-  a;
-  b;
+  a = 0; b = 0;
+  total = [];
+  totals;
+  str = "";
+
+  clear(){
+    
+  }
+
+  calc(){
+    if(Math.pow(this.a,this.b) % 2 == 1){
+      for(let i = this.b-1; i >= 0)
+    }
+    else{
+      for(let i = this.b; i < Math.pow(this.a,this.b); i++){
+        for(let j = 0; j < i; j++){
+          this.str += "";
+        }
+        this.total.push(this.str);
+        this.str = "";
+      }
+    }
+
+  }
+
 }
