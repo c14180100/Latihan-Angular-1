f531a9d59176cdb264e5e99fda383157d861d930
c14180100 <c14180100@john.petra.ac.id>
2020-04-15 10:21:51 +0700

    1 file modified - ganjil berhasil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent  {
   name = 'Angular';
 
   a = 0; b = 0;
+  x = 0; y = 0;
   total = [];
   totals;
   str = "";
@@ -19,9 +20,34 @@ export class AppComponent  {
 
   calc(){
     if(Math.pow(this.a,this.b) % 2 == 1){
-      for(let i = this.b-1; i >= 0; i--){
-        
+      var x = this.a;
+      var y = this.b;
+      if(this.b > this.a){
+        for(let i = 1; i <= this.b; i++){
+          for(let j = 0; j < i; j++){
+            this.str += "*";
+          }
+          for(let j = 0; j < x; j++){
+            this.str += "o";
+          }
+          x--;
+          this.total.push(this.str);
+          this.str = "";
+        }
+      } else{
+        for(let i = 1; i <= this.a; i++){
+          for(let j = 0; j < i; j++){
+            this.str += "*";
+          }
+          for(let j = 0; j < y; j++){
+            this.str += "o";
+          }
+          y--;
+          this.total.push(this.str);
+          this.str = "";
+        }
       }
+
     }
     else{
       for(let i = this.b; i < Math.pow(this.a,this.b); i++){
